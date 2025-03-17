ad14978e47c1b0d6490a12b28c1c96fe8fdc2be6
Dino <minetest@dino0815.de>
2025-03-17 14:54:56 +0100

    Schieberegler zu SoundButtons hinzugefügt

--- soundbutton.py
+++ soundbutton.py
@@ -4,9 +4,9 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 import cairo
 
-class SoundButton(Gtk.DrawingArea):
+class SoundButton(Gtk.Box):
     def __init__(self, position=0, offset_x=0, offset_y=0):
-        super().__init__()
+        super().__init__(orientation=Gtk.Orientation.VERTICAL)
         self.position = position  # Speichere die Position
         self.offset_x = offset_x  # X-Offset für die Position
         self.offset_y = offset_y  # Y-Offset für die Position
@@ -14,12 +14,32 @@ class SoundButton(Gtk.DrawingArea):
         self.set_vexpand(False)
         self.set_hexpand(False)
         
+        # DrawingArea für den Button
+        self.drawing_area = Gtk.DrawingArea()
+        self.drawing_area.set_size_request(300, 150)  # Höhe für den Button
+        self.drawing_area.set_vexpand(False)
+        self.drawing_area.set_hexpand(False)
+        
         # Event-Handler für Zeichnen
-        self.connect("draw", self.on_draw)
+        self.drawing_area.connect("draw", self.on_draw)
         
         # Event-Handler für Mausklicks
-        self.add_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-        self.connect("button-press-event", self.on_button_press)
+        self.drawing_area.add_events(Gdk.EventMask.BUTTON_PRESS_MASK)
+        self.drawing_area.connect("button-press-event", self.on_button_press)
+        
+        # Schieberegler erstellen
+        self.scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL)
+        self.scale.set_range(0, 100)  # Wertebereich von 0 bis 100
+        self.scale.set_value(50)  # Standardwert auf 50
+        self.scale.set_draw_value(True)  # Aktuellen Wert anzeigen
+        self.scale.set_vexpand(False)
+        self.scale.set_hexpand(False)
+        self.scale.set_size_request(200, 30)  # Breite und Höhe des Schiebereglers
+        self.scale.connect("value-changed", self.on_scale_changed)
+        
+        # Widgets zum Container hinzufügen
+        self.pack_start(self.drawing_area, False, False, 0)
+        self.pack_start(self.scale, False, False, 5)  # 5px Abstand
         
         # Debug-Ausgabe
         print(f"SoundButton {self.position + 1} erstellt - Position: x={self.offset_x}, y={self.offset_y}")
@@ -30,7 +50,7 @@ class SoundButton(Gtk.DrawingArea):
         self.offset_x = offset_x
         self.offset_y = offset_y
         print(f"SoundButton {self.position + 1} - Offset aktualisiert: x={self.offset_x}, y={self.offset_y}")
-        self.queue_draw()  # Neu zeichnen bei Änderung
+        self.drawing_area.queue_draw()  # Neu zeichnen bei Änderung
     
     def rounded_rectangle(self, cr, x, y, width, height, radius):
         # Hilfsfunktion zum Zeichnen eines abgerundeten Rechtecks
@@ -46,19 +66,19 @@ class SoundButton(Gtk.DrawingArea):
     def on_draw(self, widget, cr):
         # Abgerundetes Rechteck als Hintergrund zeichnen
         cr.set_source_rgb(0.8, 0.8, 0.8)  # Hellgrau
-        self.rounded_rectangle(cr, 0, 0, 300, 200, 15)  # Radius 15px
+        self.rounded_rectangle(cr, 0, 0, 300, 150, 15)  # Radius 15px
         cr.fill()
         
         # Einen Button-ähnlichen Kreis zeichnen
         cr.set_source_rgb(0.2, 0.4, 0.8)  # Blau
-        cr.arc(150, 100, 50, 0, 2 * 3.14159)
+        cr.arc(150, 75, 50, 0, 2 * 3.14159)
         cr.fill()
         
         # Text auf dem Button
         cr.set_source_rgb(1, 1, 1)  # Weiß
         cr.select_font_face("Sans", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
         cr.set_font_size(20)
-        cr.move_to(130, 110)
+        cr.move_to(130, 85)
         cr.show_text(f"Button {self.position + 1}")
         
         return False
@@ -67,7 +87,7 @@ class SoundButton(Gtk.DrawingArea):
         # Berechne die Distanz vom Klickpunkt zum Zentrum des Buttons
         # Die Klickkoordinaten sind bereits relativ zum Button
         center_x = 150  # Zentrum des Buttons
-        center_y = 100
+        center_y = 75
         distance = ((event.x - center_x) ** 2 + (event.y - center_y) ** 2) ** 0.5
         
         # Wenn der Klick innerhalb des Buttons war
@@ -77,4 +97,9 @@ class SoundButton(Gtk.DrawingArea):
         else:
             print(f"Außerhalb von Button {self.position + 1} geklickt: x={event.x}, y={event.y}")
         
-        return True 
+        return True
+    
+    def on_scale_changed(self, scale):
+        """Callback für Änderungen am Schieberegler"""
+        value = scale.get_value()
+        print(f"Button {self.position + 1} - Schieberegler auf {value} gesetzt") 
